ab68d50a513b92733d85248c32f48aff92d3b287
taogen <taogenjia@gmail.com>
2020-07-17 18:02:16 +0800

    Add redis

--- src/main/java/com/taogen/App.java
+++ src/main/java/com/taogen/App.java
@@ -2,8 +2,13 @@ package com.taogen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
+
+import javax.annotation.Resource;
+import java.util.Date;
 
 /**
  * Hello world!
@@ -12,12 +17,34 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class App{
+
+    private Jedis jedis = new Jedis("localhost", 6379);
+
+    @Resource
+    private RedisTemplate<String, String> redisTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
     @GetMapping("/")
     public String hello()
     {
         return "hello! - " + System.currentTimeMillis();
     }
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+
+    @GetMapping("/add")
+    public String add(){
+        try {
+            jedis.sadd("dates", new Date().toString());
+        } catch (Exception e){
+            System.out.println(e.getClass().getName() + ":" + e.getMessage());
+        }
+        return "ok";
+    }
+
+    @GetMapping("/list")
+    public String list(){
+        return jedis.smembers("dates").toString();
     }
 }
