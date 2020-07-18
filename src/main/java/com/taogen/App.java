638083c6a535a1d6d89be210c2db8d9fdc467500
taogen <taogenjia@gmail.com>
2020-07-18 13:33:32 +0800

    Set redis pswd in docker

--- src/main/java/com/taogen/App.java
+++ src/main/java/com/taogen/App.java
@@ -2,49 +2,47 @@ package com.taogen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import redis.clients.jedis.Jedis;
 
-import javax.annotation.Resource;
 import java.util.Date;
 
 /**
  * Hello world!
- *
  */
 @SpringBootApplication
 @RestController
-public class App{
+public class App {
 
     private Jedis jedis = new Jedis("redis", 6379);
-
-    @Resource
-    private RedisTemplate<String, String> redisTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
+    public Jedis getJedis() {
+        jedis.auth("yourpassword");
+        return jedis;
+    }
+
     @GetMapping("/")
-    public String hello()
-    {
+    public String hello() {
         return "hello! - " + System.currentTimeMillis();
     }
 
     @GetMapping("/add")
-    public String add(){
+    public String add() {
         try {
-            jedis.sadd("dates", new Date().toString());
-        } catch (Exception e){
+            getJedis().sadd("dates", new Date().toString());
+        } catch (Exception e) {
             System.out.println(e.getClass().getName() + ":" + e.getMessage());
         }
         return "ok";
     }
 
     @GetMapping("/list")
-    public String list(){
-        return jedis.smembers("dates").toString();
+    public String list() {
+        return getJedis().smembers("dates").toString();
     }
 }
